7deb84e0ed488b617f4195a4982bcfbf49decde0
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2023-12-19 10:29:47 +0900

    remove: pyserial for melodic

--- install.sh
+++ install.sh
@@ -12,12 +12,10 @@ function main(){
     # git clone https://github.com/KMiyawaki/oit_roboclaw_driver2.git
     sudo apt-get install -y ros-${TARGET_ROS}-teleop-twist-keyboard
     sudo apt-get install -y ros-${TARGET_ROS}-key-teleop
+    sudo apt-get install -y ros-${TARGET_ROS}-cartographer-ros
     sudo apt-get install -y xterm
-    if [[ $TARGET_ROS = "melodic" ]]; then
-        sudo /usr/bin/python2 -m pip install pyserial
-    else
-        sudo /usr/bin/python3 -m pip install pyserial
-    fi
+        
+    sudo /usr/bin/python3 -m pip install pyserial
     cd ${HOME}/ros2_ws/src/oit_minibot_light_01_ros2
     ./build.sh -a
 }
