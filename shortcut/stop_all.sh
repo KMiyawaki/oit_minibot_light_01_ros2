ef9987c3de0c1bb9bfb8b5ea1295ec80e1db38bf
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2024-02-19 19:07:13 +0900

    add: publish zero twist

--- shortcut/stop_all.sh
+++ shortcut/stop_all.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 function main(){
+    local -r PACKAGE="oit_minibot_light_01_ros2"
+    local -r DIST=`${HOME}/ros2_ws/src/${PACKAGE}/get_suitable_ros2.sh`
+    source /opt/ros/${DIST}/setup.bash
+    source ${HOME}/ros2_ws/install/setup.bash
+    export ROS_LOCALHOST_ONLY=1
+    export ROS_DOMAIN_ID=0
+    
+    ros2 topic pub -r 2 -t 5 /cmd_vel geometry_msgs/Twist '{linear: {x: 0.0}, angular: {z: 0.0}}'
+
     local PROGS=`ps -aux | grep ros | grep -v grep | awk '{ printf("%d ", $2);for(i=11;i<=NF;++i){ printf("%s ",$i) };printf("\n") }'`
     while read p
     do
